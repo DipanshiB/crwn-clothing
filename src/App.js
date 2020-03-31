c642a634cfb999809a11fb271977f58f1b06ff4f
Dipanshi Bansal <dipanshibansal@gmail.com>
2020-03-31 23:22:04 +0530

    added custom input & button + styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import AuthPage from './pages/auth/auth.component';
 import Header from './components/header/header.component';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={AuthPage} />
         <HomePage />
       </Switch>
     </div>
---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './auth.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const AuthPage = () => (
+    <div className = 'auth'>
+        <SignIn />
+    </div>
+)
+
+export default AuthPage;
