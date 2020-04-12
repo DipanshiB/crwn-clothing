b6d2fddb6c92036d9a586619fef822999fd48c09
Dipanshi Bansal <dipanshibansal@gmail.com>
2020-04-12 19:30:18 +0530

    implemented userReducer, userActions to header, app components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import AuthPage from './pages/auth/auth.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 // import { render } from 'node-sass';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser : {
-              id : snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id : snapShot.id,
+            ...snapShot.data()
           })
         })
       }
-      else{
-        this.setState({currentUser : userAuth})
-
-      }
+        setCurrentUser(userAuth);
     })
   }
 
@@ -59,4 +51,8 @@ class App extends React.Component {
 
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser : user => dispatch(setCurrentUser(user)) //dispatch method tells that you gotta return an action as the payload
+})
+
+export default connect(null, mapDispatchToProps)(App);
